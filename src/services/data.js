b50aa2e8ba8366986428dd1faf45c65f290c22b3
richardwellerson <richardwellerson@gmail.com>
2020-07-02 19:41:18 -0300

    Estrutura de dados criada

---
+++ src/services/data.js
@@ -0,0 +1,174 @@
+export const user = [
+  {
+    name: 'Nayara Nascimento',
+    email: '@gmail.com',
+    password: 'password',
+    image: 'https://w7.pngwing.com/pngs/34/886/png-transparent-female-woman-girl-computer-icons-svg-face-people-logo.png',
+  },
+  {
+    name: 'Diego Faria',
+    email: '@gmail.com',
+    password: 'password',
+    image: 'https://w7.pngwing.com/pngs/34/886/png-transparent-female-woman-girl-computer-icons-svg-face-people-logo.png',
+  },
+  {
+    name: 'Richard Martins',
+    email: '@gmail.com',
+    password: 'password',
+    image: 'https://w7.pngwing.com/pngs/34/886/png-transparent-female-woman-girl-computer-icons-svg-face-people-logo.png',
+  },
+  {
+    name: 'Henrique Baêta',
+    email: '@gmail.com',
+    password: 'password',
+    image: 'https://w7.pngwing.com/pngs/34/886/png-transparent-female-woman-girl-computer-icons-svg-face-people-logo.png',
+  }
+];
+
+export const allBeers = [
+  {
+    beer: 'Adriática',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_adriatica.jpg',
+  },
+  {
+    beer: 'Antarctica',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_antarctica.jpg',
+  },
+  {
+    beer: 'Beck´s',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_becks.jpg',
+  },
+  {
+    beer: 'Bohemia',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_bohemia.jpg',
+  },
+  {
+    beer: 'Brahma',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_brahma.jpg',
+  },
+  {
+    beer: 'Budweiser',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_bud.jpg',
+  },
+  {
+    beer: 'Caracu',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_caracu.jpg',
+  },
+  {
+    beer: 'Cervejaria Colorado',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_colorado.jpg',
+  },
+  {
+    beer: 'Corona',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_corona.jpg',
+  },
+  {
+    beer: 'Franziskaner',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_franziskaner.jpg',
+  },
+  {
+    beer: 'Goose Island',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_goose-island.jpg',
+  },
+  {
+    beer: 'Hertog Jan',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_hertog-jan.jpg',
+  },
+  {
+    beer: 'Hoegaarden',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_hoegaarden.jpg',
+  },
+  {
+    beer: 'Leffe',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_leffe.jpg',
+  },
+  {
+    beer: 'Legítima',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/11/logo_legitima.jpg',
+  },
+  {
+    beer: 'Löwenbräu',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_lowen.jpg',
+  },
+  {
+    beer: 'Magnífica do Maranhão',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_magnifica-1.jpg',
+  },
+  {
+    beer: 'Norteña',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_nortena.jpg',
+  },
+  {
+    beer: 'Nossa',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_nossa.jpg',
+  },
+  {
+    beer: 'Original',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_original.jpg',
+  },
+  {
+    beer: 'Patagonia',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_patagonia.jpg',
+  },
+  {
+    beer: 'Polar',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_polar-novo.jpg',
+  },
+  {
+    beer: 'Quilmes',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_quilmes.jpg',
+  },
+  {
+    beer: 'Serramalte',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_serramalte.jpg',
+  },
+  {
+    beer: 'Serrana',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_serrana.jpg',
+  },
+  {
+    beer: 'Skol',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_skol.jpg',
+  },
+  {
+    beer: 'Stella Artois',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_stella-artois.jpg',
+  },
+  {
+    beer: 'Três Fidalgas',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_tres-fidalgas.jpg',
+  },
+  {
+    beer: 'Wäls',
+    price: 15.5,
+    image: 'https://www.ambev.com.br/conteudo/uploads/2019/03/logo_wals.jpg',
+  },
+];
