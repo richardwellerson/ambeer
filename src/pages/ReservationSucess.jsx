dc2f0c9b39f33e4ac82e908b90fecf3b8e8e6a23
henrique <hbaetaleite@gmail.com>
2020-07-03 18:13:25 -0300

    Criando page Fechamento

---
+++ src/pages/ReservationSucess.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+const ReservationSucess = () => {
+  return (
+    <div>
+      <h3>Rserva realizada com sucesso!</h3>
+      <h4>ESPERAMOS POR VOCÊd</h4>
+      <Footer />
+    </div>
+  );
+};
+
+export default ReservationSucess;
