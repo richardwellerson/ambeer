96055d745af14c1f3c4218e0a54d5a800968d8da
richardwellerson <richardwellerson@gmail.com>
2020-07-04 19:35:00 -0300

    Pagina completa. Faltando estilizaçao

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Box, Avatar } from '@material-ui/core';
 import profileIcon from '../../img/profile.svg';
 import exitIcon from '../../img/exit.svg';
 import menuIcon from '../../img/menu.svg';
 import '../../styles/Footer.css';
 
-const cleanAndExit = () => {
-  localStorage.clear();
-  return <Redirect to="/" />;
+const cleanAndExit = (history) => {
+  localStorage.clear(); 
+  alert('Logoff realizado. ');
+  history.push('/');
 };
 
-const Footer = () => (
-  <Box className="Footer_all">
-    <Link to="/profile">
-      <Avatar alt="link para Perfil" src={profileIcon} />
-    </Link>
-    <Link to="/menu">
-      <Avatar alt="link para Cardápio" src={menuIcon} />
-    </Link>
-    <Box onClick={() => cleanAndExit()}>
-      <Avatar alt="sair da aplicação" src={exitIcon} />
+const Footer = () => {
+  const history = useHistory();
+  return (
+    <Box className="Footer_all">
+      <Link to="/profile">
+        <Avatar alt="link para Perfil" src={profileIcon} />
+      </Link>
+      <Link to="/menu">
+        <Avatar alt="link para Cardápio" src={menuIcon} />
+      </Link>
+      <Box onClick={() => cleanAndExit(history)}>
+        <Avatar alt="sair da aplicação" src={exitIcon} />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default Footer;
