c99ba617302fe953fc44c487c4ba0a81b011cd22
henrique <hbaetaleite@gmail.com>
2020-07-03 22:40:52 -0300

    usando context

--- src/components/Profile/UserData.jsx
+++ src/components/Profile/UserData.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Box, Typography } from '@material-ui/core';
+import Ambeer from '../../context';
 
 const UserData = () => {
+  const { userData } = useContext(Ambeer);
   return (
     <Box>
       <img src="" alt="foto de perfil" />
-      <Typography variant="h3">UserName</Typography>
-      <Typography variant="h5">email</Typography>
+      <Typography variant="h3">{userData.name}</Typography>
+      <Typography variant="h5">{userData.email}</Typography>
     </Box>
   );
 };
