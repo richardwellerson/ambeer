e2ad32ff226652e43dae7434ce5362121f6616e9
henrique <hbaetaleite@gmail.com>
2020-07-04 19:56:46 -0300

    codeclimate

--- src/components/Profile/ProfileTitle.jsx
+++ src/components/Profile/ProfileTitle.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 
 export default function ProfileTitle() {
   return (
     <Box>
-      <p>Profile screen</p>
+      <Typography>Profile screen</Typography>
     </Box>
   );
 }
