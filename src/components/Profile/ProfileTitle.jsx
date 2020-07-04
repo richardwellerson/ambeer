4d4b77ef774f2fc7ea5f6bec4842ca5eeb3a9702
henrique <hbaetaleite@gmail.com>
2020-07-04 19:47:44 -0300

    correcao sugerida pelo Diego

--- src/components/Profile/ProfileTitle.jsx
+++ src/components/Profile/ProfileTitle.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
 
 export default function ProfileTitle() {
   return (
-    <div>
+    <Box>
       <p>Profile screen</p>
-    </div>
+    </Box>
   );
 }
