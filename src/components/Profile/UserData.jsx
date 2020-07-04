6a5430821df03e97bd7943417b4dcf9fa7ceb640
henrique <hbaetaleite@gmail.com>
2020-07-03 22:47:47 -0300

    CodeCLimate

--- src/components/Profile/UserData.jsx
+++ src/components/Profile/UserData.jsx
@@ -3,12 +3,14 @@ import { Box, Typography } from '@material-ui/core';
 import Ambeer from '../../context';
 
 const UserData = () => {
-  const { userData } = useContext(Ambeer);
+  const {
+    userData: { name, email },
+  } = useContext(Ambeer);
   return (
     <Box>
       <img src="" alt="foto de perfil" />
-      <Typography variant="h3">{userData.name}</Typography>
-      <Typography variant="h5">{userData.email}</Typography>
+      <Typography variant="h3">{name}</Typography>
+      <Typography variant="h5">{email}</Typography>
     </Box>
   );
 };
