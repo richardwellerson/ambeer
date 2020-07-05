efff3aeb4ae9956e02a981a5869f3d1e9c08cff0
nayararn <nayaranascimento0112@gmail.com>
2020-07-04 23:41:14 -0300

    estilização da tela de reserva finalizada

--- src/components/Reservation/ReservationTitle.jsx
+++ src/components/Reservation/ReservationTitle.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Typography, Box } from '@material-ui/core';
+import React from "react";
+import { Typography, Box } from "@material-ui/core";
+import "../../styles/index.css";
 
 const ReservationTitle = () => (
   <>
-    <Box>
-      <Typography variant="h3">Reserva</Typography>
-      <Typography variant="h4">Escolha o seu dia</Typography>
+    <Box className="titulo-reserva">
+      <Typography variant="h4">Reserva</Typography>
+      <Typography variant="subtitle1">Escolha o seu dia</Typography>
     </Box>
   </>
 );
 
-export default ReservationTitle
+export default ReservationTitle;
--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
+import React from "react";
+import { Box } from "@material-ui/core";
 
-import ReservationButton from '../components/Reservation/ReservationButton';
-import ReservationTitle from '../components/Reservation/ReservationTitle';
-import ButtonPreOrder from '../components/Reservation/ButtonPreOrder';
-import FinishReservation from '../components/Reservation/FinishReservation';
-import PikersDate from '../components/Reservation/PikersDate';
+import ReservationButton from "../components/Reservation/ReservationButton";
+import ReservationTitle from "../components/Reservation/ReservationTitle";
+import ButtonPreOrder from "../components/Reservation/ButtonPreOrder";
+import FinishReservation from "../components/Reservation/FinishReservation";
+import PikersDate from "../components/Reservation/PikersDate";
+import "../styles/index.css";
 
 const Reservation = () => (
-  <>
-    <Box>
+  <Box className="reserva">
+    <Box className="container-titulo-reserva">
       <ReservationTitle />
       <PikersDate />
+    </Box>
+    <Box className="container-botoes-reserva">
       <ButtonPreOrder />
       <FinishReservation />
       <ReservationButton />
     </Box>
-  </>
+  </Box>
 );
 
 export default Reservation;
