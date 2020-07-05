414762fe0c3343e39212ba8751a19d8ebe6c5041
nayararn <nayaranascimento0112@gmail.com>
2020-07-05 14:06:26 -0300

    tela de reserva finalizada

--- src/components/Menu/InputSearch.jsx
+++ src/components/Menu/InputSearch.jsx
@@ -1,16 +1,15 @@
-import React, { useContext } from 'react';
-import { Box, TextField } from '@material-ui/core';
-import Ambeer from '../../context';
+import React, { useContext } from "react";
+import { Box, TextField } from "@material-ui/core";
+import Ambeer from "../../context";
 
 const InputSearch = () => {
   const { setUserSearch } = useContext(Ambeer);
   return (
-    <Box>
-      <TextField
+    <Box className="busca-cardapio">
+      <input
         onKeyUp={({ target }) => setUserSearch(target.value)}
         id="outlined-basic"
-        label="Produto"
-        variant="outlined"
+        placeholder="Buscar produto"
       />
     </Box>
   );
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-import FilterProducts from '../components/Menu/FilterProducts';
-import ShowProducts from '../components/Menu/ShowProducts';
-import InputSearch from '../components/Menu/InputSearch';
-import MenuTitle from '../components/Menu/MenuTitle';
-import Footer from '../components/Footer/Footer';
-import FinishPreOrder from '../components/Menu/FinishPreOrder';
+import React from "react";
+import { Box } from "@material-ui/core";
+import FilterProducts from "../components/Menu/FilterProducts";
+import ShowProducts from "../components/Menu/ShowProducts";
+import InputSearch from "../components/Menu/InputSearch";
+import MenuTitle from "../components/Menu/MenuTitle";
+import Footer from "../components/Footer/Footer";
+import FinishPreOrder from "../components/Menu/FinishPreOrder";
 
 const Menu = () => (
-  <Box>
-    <MenuTitle />
-    <InputSearch />
-    <FilterProducts />
-    <FinishPreOrder />
+  <Box className="cardapio">
+    <Box className="container-cardapio">
+      <MenuTitle />
+      <InputSearch />
+      <FilterProducts />
+    </Box>
     <ShowProducts />
     <Footer />
   </Box>
