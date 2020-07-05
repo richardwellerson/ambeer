8263a4daa9deb647d290bd057af479d092749842
DiegoRafaelAF <draf0314@gmail.com>
2020-07-05 13:30:26 -0300

    começando testes aplicaçao

--- src/tests/LoginScreen.test.js
+++ src/tests/LoginScreen.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { cleanup, fireEvent, act } from '@testing-library/react';
+import { cleanup, fireEvent, act, waitForElement } from '@testing-library/react';
 import renderWithRouter from './mocks/RenderService';
 import 'jest-mock';
 import Login from '../pages/Login';
 import Ambeer from '../context';
+import Profile from '../pages/Profile';
 
-const saveInput = jest.fn();
+const  saveInput = jest.fn();
+const  userData = jest.fn();
+const mocks = { saveInput, userData };
 
 describe('Testing login screen', () => {
   afterEach(cleanup);
@@ -26,8 +29,8 @@ describe('Testing login screen', () => {
   test('should check dataTestId', () => {
     const { getByTestId } = renderWithRouter(
       <Ambeer.Provider value={ saveInput }>
-      <Login />
-    </Ambeer.Provider>
+        <Login />
+      </Ambeer.Provider>
     );
 
     act(() => {
@@ -41,49 +44,60 @@ describe('Testing login screen', () => {
     });
   });
 
-  test('should check event', () => {
-    const { getByTestId, getByRole } = renderWithRouter(
-      <Ambeer.Provider value={ saveInput }>
-      <Login />
-    </Ambeer.Provider>
+  test('should check event', async () => {
+    const { getByTestId, history } = renderWithRouter(
+      <Ambeer.Provider value={ mocks }>
+        <Login />
+        <Profile />
+      </Ambeer.Provider>
     );
 
-    act(() => {
-      const email = 'test@gmail.com';
-      const password = '123456';
-      const inputEmail = getByTestId('email-input');
-      const inputPassword = getByTestId('password-input');
+    const [
+      email,
+      password,
+      inputEmail,
+      inputPassword,
+      inputButton,
+    ] = await waitForElement(() => [
+      'test@gmail.com',
+      '123456',
+      getByTestId('email-input'),
+      getByTestId('password-input'),
+      getByTestId('login-submit-btn'),
+    ]);
 
+    act(() => {
       fireEvent.input(inputEmail, {
         target: { value: email },
       });
       fireEvent.input(inputPassword, {
         taget: { value: password },
       });
-      expect(getByRole('button', { disabled: false }));
+      fireEvent.click(inputButton);
+      expect(history.location.pathname).toEqual('/profile');
     });
   });
 
-  test('testing false results', () => {
-    const { getByTestId, getByRole } = renderWithRouter(
-      <Ambeer.Provider value={ saveInput }>
-      <Login />
-    </Ambeer.Provider>
-    );
+  // test('testing false results', () => {
+  //   const { getByTestId, getByRole } = renderWithRouter(
+  //     <Ambeer.Provider value={ saveInput }>
+  //     <Login />
+  //   </Ambeer.Provider>
+  //   );
 
-    act(() => {
-      const email = 'testgmail.com';
-      const password = '12345';
-      const inputEmail = getByTestId('email-input');
-      const inputPassword = getByTestId('password-input');
+  //   act(() => {
+  //     const email = 'testgmail.com';
+  //     const password = '12345';
+  //     const inputEmail = getByTestId('email-input');
+  //     const inputPassword = getByTestId('password-input');
 
-      fireEvent.input(inputEmail, {
-        target: { value: email },
-      });
-      fireEvent.input(inputPassword, {
-        taget: { value: password },
-      });
-      expect(getByRole('button', { disabled: true }));
-    });
-  });
+  //     fireEvent.input(inputEmail, {
+  //       target: { value: email },
+  //     });
+  //     fireEvent.input(inputPassword, {
+  //       taget: { value: password },
+  //     });
+  //     expect(getByRole('input', { disabled: true }));
+  //   });
+  // });
 });
