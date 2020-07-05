292ca0eb0f5c5484261608019b4ddb1af569daaf
nayararn <nayaranascimento0112@gmail.com>
2020-07-04 21:28:46 -0300

    tela de cadastro finalizada, ajustes na tela de login e alteração nas rotas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,30 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Typography, Box, Button } from '@material-ui/core';
-import { Redirect } from 'react-router-dom';
-import Ambeer from '../context';
-import '../styles/index.css';
+import React, { useContext, useEffect, useState } from "react";
+import { Typography, Box, Button } from "@material-ui/core";
+import { Redirect, Link } from "react-router-dom";
+import Ambeer from "../context";
+import "../styles/index.css";
 
 const handleChangeInput = (name, event, input, setInput) => {
   setInput({ ...input, [name]: event });
 };
 
 const handleFormSubmit = (saveInput, input) => {
-  localStorage.setItem('user', JSON.stringify({ email: input.email }));
+  localStorage.setItem("user", JSON.stringify({ email: input.email }));
 
   saveInput(input);
 };
 
 const Login = () => {
   const { saveInput } = useContext(Ambeer);
-  const [input, setInput] = useState({ email: '', password: '' });
+  const [input, setInput] = useState({ email: "", password: "" });
   const [informations, setInformations] = useState(true);
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
     const validEmailRegEx = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i;
 
-    if (!validEmailRegEx.test(input.email)
-      || (input.password.length <= 5)) return setInformations(true);
+    if (!validEmailRegEx.test(input.email) || input.password.length <= 5)
+      return setInformations(true);
 
     return setInformations(false);
   }, [input]);
@@ -43,7 +43,9 @@ const Login = () => {
             <input
               data-testid="email-input"
               type="email"
-              onChange={(ele) => handleChangeInput('email', ele.target.value, input, setInput)}
+              onChange={(ele) =>
+                handleChangeInput("email", ele.target.value, input, setInput)
+              }
             />
           </Box>
           <Box className="input">
@@ -51,7 +53,9 @@ const Login = () => {
             <input
               data-testid="password-input"
               type="password"
-              onChange={(ele) => handleChangeInput('password', ele.target.value, input, setInput)}
+              onChange={(ele) =>
+                handleChangeInput("password", ele.target.value, input, setInput)
+              }
             />
           </Box>
           <Box className="botao-login">
@@ -64,6 +68,11 @@ const Login = () => {
             >
               Entrar
             </Button>
+          </Box>
+          <Box className="botao-voltar-login">
+            <Link to="/home">
+              <Button>Voltar</Button>
+            </Link>
           </Box>
         </Box>
       </Box>
